1fd2d6a86d820a6c8fcb239d55f855eedff2ca46
Riko Eksteen <reksteen@gmail.com>
2022-06-30 18:54:37 +0100

    chore: prettier formatting

--- web/stopwatch-react/src/utils.js
+++ web/stopwatch-react/src/utils.js
@@ -1,12 +1,12 @@
 export function zeroPad(number, places) {
-  return number.toString().padStart(places, "0");
+  return number.toString().padStart(places, '0')
 }
 
 export function formatTime(milliseconds) {
-  const date = new Date(milliseconds);
-  const mins = date.getMinutes();
-  const secs = date.getSeconds();
-  const centis = Math.floor(date.getMilliseconds() / 10);
+  const date = new Date(milliseconds)
+  const mins = date.getMinutes()
+  const secs = date.getSeconds()
+  const centis = Math.floor(date.getMilliseconds() / 10)
 
-  return `${zeroPad(mins, 2)}:${zeroPad(secs, 2)}.${zeroPad(centis, 2)}`;
+  return `${zeroPad(mins, 2)}:${zeroPad(secs, 2)}.${zeroPad(centis, 2)}`
 }
