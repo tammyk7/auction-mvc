f40ccb517a7636195eed61cf7555afac83b0f83b
Amity Black <93537588+ablackadaptive@users.noreply.github.com>
2023-02-01 17:10:19 -0500

    feat: initial version (#7)

---
+++ java/auctionhouse-console/initial/src/main/java/com/weareadaptive/auctionhouse/AuctionhouseConsoleApplication.java
@@ -0,0 +1,13 @@
+package com.weareadaptive.auctionhouse;
+
+/**
+ * Hello world!
+ *
+ */
+public class AuctionhouseConsoleApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
