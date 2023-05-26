67399f1f2fdb370258cbdabfc93e44b2c49a4832
Rafael Garcia <rafael.garcia@weareadaptive.com>
2023-05-26 15:25:00 -0400

    add challenge-2 for wk1d2

---
+++ web/manila-training/week-1/Day-2/challenge-2/src/App.tsx
@@ -0,0 +1,55 @@
+import React, { useRef, useReducer, useEffect } from 'react';
+import "./App.css";
+
+interface State {
+  duration: number;
+  remainingTime: number;
+  isRunning: boolean;
+}
+
+const initialState: State = {
+  duration: 0,
+  remainingTime: 0,
+  isRunning: false,
+};
+
+const reducer = (state: State, action: Action): State => {
+  // TODO
+};
+
+const CountdownTimer: React.FC = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    // TODO
+  }, [state.isRunning, state.remainingTime]);
+
+  const startTimer = (): void => {
+    // TODO
+    alert("Start Timer");
+  };
+  
+  const stopTimer = (): void => {
+    // TODO
+    alert("Stop Timer");
+  };
+
+  return (
+    <div className="wrapper">
+      <h1>Countdown Timer</h1>
+      <input type="number" />
+      <div>
+        <button onClick={startTimer}>Start</button>
+        <button onClick={stopTimer}>Stop</button>
+      </div>
+      {state.isRunning ? (
+        <p>Time Remaining: {state.remainingTime} seconds</p>
+      ) : (
+        <p>Enter a duration and start the timer.</p>
+      )}
+      {state.remainingTime === 0 && <p>Timer Ended!</p>}
+    </div>
+  );
+};
+
+export default CountdownTimer;
